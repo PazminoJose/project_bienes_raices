01d3c0c9c262c67c8b42a269ac7429d7f5f80841
José Pazmiño <pepe.paz0399@gmail.com>
2022-04-04 20:51:40 -0500

    añadir control de  exception a la conexión

--- includes/config/database.php
+++ includes/config/database.php
@@ -7,7 +7,7 @@ function conectarDB(): mysqli
         $db = new mysqli($_ENV['DB_HOST'], $_ENV['DB_USER'], $_ENV['DB_PASSWORD'], $_ENV['DB_BD']);
         return $db;
     } catch (mysqli_sql_exception $e) {
-        echo "Error no se pudo conectar " . $e;
+        echo "Error no se pudo conectar " . $db->errno;
         exit;
     }
   
